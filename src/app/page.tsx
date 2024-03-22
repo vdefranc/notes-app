573f33ed961e39a4d91af205c4e266c98ae417c0
Vincent DeFrancesco <vincentg.defrancesco@gmail.com>
2024-03-22 13:40:07 -0400

    add prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,8 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-
 export default function Home() {
   return (
     <main>
       <div>
-        <p>
-          Hey friends :)
-        </p>
+        <p>Hey friends :)</p>
       </div>
     </main>
   );
