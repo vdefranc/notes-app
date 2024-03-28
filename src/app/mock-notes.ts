86ac1ff2390c553d7e029038739e8042b3c14910
Vincent DeFrancesco <vincentg.defrancesco@gmail.com>
2024-03-27 21:44:39 -0400

    fix lint issue

--- src/app/mock-notes.ts
+++ src/app/mock-notes.ts
@@ -11,8 +11,8 @@ notes.push(
         user_id: `${i}`,
         title: `note ${i}`,
         body: "Arugula bell bok broccoli bunya carrot caulie chestnut chicory collard courgette cucumber epazote garbanzo green. Amaranth artichoke avocado azuki black-eyed bok broccoli brussels burdock cabbage celery celtuce chestnut chickpea chicory choy collard cress daikon desert dulse earthnut epazote fennel garlic greens. Artichoke arugula bean beet beetroot bitterleaf black-eyed bona broccoli bunya bush cabbage carrot caulie celery chard chicory choy daikon dandelion desert dulse epazote fennel gourd green.",
-        created_at: new Date().toISOString(),
-        updated_at: new Date().toISOString(),
+        created_at: new Date(),
+        updated_at: new Date(),
       };
     }),
 );
